34a6ba01b26d063c0d99cce4404d849f18f5854a
opannapo <taufan.alfazri@go-work.com>
2020-11-11 01:04:59 +0700

    move --> project location

--- napo-elastic/napoelastic/repository/repositories.go
+++
@@ -1,6 +0,0 @@
-package repository
-
-type IndexRepository interface {
-	GetInfo() (result interface{}, err error)
-	GetCat() (result interface{}, err error)
-}
--- napo-elastic/napoelastic/services/services.go
+++
@@ -1,6 +0,0 @@
-package services
-
-type IndexService interface {
-	GetInfo() (result interface{}, err error)
-	GetCat() (result interface{}, err error)
-}
